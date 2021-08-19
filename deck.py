d8f02403c0e211b6edecf7db7ce4088993bdbbc7
Waqas P <67988358+waqaspathan00@users.noreply.github.com>
2021-08-19 15:06:41 -0400

    removed code redundancies

--- deck.py
+++ deck.py
@@ -23,22 +23,16 @@ class Deck:
     RANKS = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13,
              'A': 14}
 
-    red_heart = []
-    red_diamond = []
-    black_clover = []
-    black_spade = []
-
     deck = []
     full_deck = []
 
     def __init__(self):
         for RANK in self.RANKS.keys():
-            self.red_heart.append(Deck.Card(RANK, 'red hearts'))
-            self.red_diamond.append(Deck.Card(RANK, 'red diamonds'))
-            self.black_clover.append(Deck.Card(RANK, 'black clubs'))
-            self.black_spade.append(Deck.Card(RANK, 'black spades'))
+            self.deck.append(Deck.Card(RANK, 'red hearts'))
+            self.deck.append(Deck.Card(RANK, 'red diamonds'))
+            self.deck.append(Deck.Card(RANK, 'black clubs'))
+            self.deck.append(Deck.Card(RANK, 'black spades'))
 
-        self.deck = self.red_heart + self.red_diamond + self.black_clover + self.black_spade
         random.shuffle(self.deck)
 
         # create an extra deck to refill the main deck
